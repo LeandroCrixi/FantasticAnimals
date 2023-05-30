f44d646bde96f8279de23a8037c9ff63ab558044
Leandro Crixi <leandro.crixi@hotmail.com>
2023-05-30 20:04:04 +0100

    Commit 20230530

--- script.js
+++ script.js
@@ -44,5 +44,50 @@ function initAccordion(){
     }
 }
 
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+    function scrollToSection(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+
+        // Forma Alternativa
+        // const topo = section.offsetTop
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth',
+        // })
+    }
+
+    linksInternos.forEach((link) =>{
+        link.addEventListener('click', scrollToSection)
+    })
+}
+
+
+function initAnimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll')
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.6;
+        function animaScroll(){
+            sections.forEach((section) =>{
+                const sectionTop = section.getBoundingClientRect().top - windowMetade;
+                if(sectionTop < 0){
+                    section.classList.add('ativo')
+                }
+            })
+        }
+        animaScroll()
+        window.addEventListener('scroll', animaScroll)
+    }
+}
+
+
 initTabNav();
-initAccordion()
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll()
